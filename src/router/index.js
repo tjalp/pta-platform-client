be6509048e03d5f2e64990a5514cf96dd5b4f4ad
tjalp <60233996+tjalp@users.noreply.github.com>
2024-09-07 18:17:45 +0200

    improvement: lazy-load pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import PtaView from '@/views/PtaView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/:pathMatch(.*)*', name: 'notfound', component: NotFoundView },
+    { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('../views/NotFoundView.vue') },
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/pta/:id',
       name: 'pta',
-      component: PtaView
+      component: () => import('../views/PtaView.vue')
     },
     {
       path: '/dashboard',
