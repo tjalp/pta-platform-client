4037553b908172b6c58491167895b93c4fe24a10
tjalp <60233996+tjalp@users.noreply.github.com>
2024-10-02 00:27:14 +0200

    feat: add text to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
-    
+    <div>
+        <Card>
+            <template #title>Welkom op het PTA Platform</template>
+            <template #content>
+                <p class="m-0">
+                    Dit platform is bedoeld voor het inzien en bewerken van Programma's van Toetsing en Afsluiting (PTA's).
+                    Om te beginnen, vul de juiste gegevens in het zoekveld in door bovenaan te klikken op 'Zoeken'.
+                </p>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <script setup>
+import Card from 'primevue/card';
 </script>
