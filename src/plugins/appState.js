fcf0ece15606fafda75093516e030c2ff3dee203
tjalp <60233996+tjalp@users.noreply.github.com>
2024-10-02 17:13:27 +0200

    fix: theme not auto applying on load

--- src/plugins/appState.js
+++ src/plugins/appState.js
@@ -3,7 +3,15 @@ import { reactive } from 'vue';
 export default {
     install: (app) => {
         const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        console.log(prefersDarkScheme);
         const _appState = reactive({ theme: 'Aura', darkTheme: prefersDarkScheme });
+        const root = document.getElementsByTagName('html')[0];
+
+        if (prefersDarkScheme) {
+            root.classList.add('p-dark');
+        } else {
+            root.classList.remove('p-dark');
+        }
 
         app.config.globalProperties.$appState = _appState;
     }
